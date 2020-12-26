135d45a50210251c11bc8af380acbb409a9a9935
Dmitry Nikolaev <dnikolaev@fastmail.com>
2020-12-26 12:59:30 +0200

    Testing code.

--- test_eurphone.py
+++ test_eurphone.py
@@ -27,3 +27,24 @@ for (vowel,) in cursor.execute(
     vowel_type_n += 1
 else:
     print(f'Success: {vowel_type_n} vowel types parsed.')
+
+print('Testing consonants...')
+consonant_type_n = 0
+for (consonant,) in cursor.execute(
+    '''
+    SELECT DISTINCT ipa FROM segments
+    WHERE `is_consonant` = 1
+    '''
+):
+    consonant = consonant.replace('(', '').replace(')', '')
+    try:
+        parser.parse(consonant)
+    except UnexpectedCharacters as e:
+        print(consonant, e)
+        input()
+    except Exception as e:
+        print(f'Other error: {consonant} {e}')
+        input()
+    consonant_type_n += 1
+else:
+    print(f'Success: {consonant_type_n} consonant types parsed.')
